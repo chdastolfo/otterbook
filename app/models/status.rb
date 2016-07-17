13f01d432facbd33ab71fddb8cfa33f88243260f
Christina D'Astolfo <chdastolfo@gmail.com>
2016-07-17 17:00:18 -0400

    modified status and status test

--- app/models/status.rb
+++ app/models/status.rb
@@ -1,4 +1,9 @@
 class Status < ActiveRecord::Base
 	#attr_accessor :content, :user_id
 	belongs_to :user
+
+	validates :content, presence: true,
+						length: { minimum: 2 }
+
+	validates :user_id, presence: true
 end
