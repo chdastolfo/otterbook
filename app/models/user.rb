39b1040b5750903eb19f007233404e2f63feb8c4
Christina D'Astolfo <chdastolfo@gmail.com>
2016-07-17 15:45:30 -0400

    fixed flash messages

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,15 @@
 
   has_many :statuses
 
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :profile_name, presence: true,
+  						   uniqueness: true,
+  						   format: {
+  						   	with: /a-zA-Z0-9_-/,
+  						   	message: 'You have to format your profile name correctly.'
+  						   }
+
   def full_name
     "#{first_name.to_s} " " #{last_name.to_s}"
   end
