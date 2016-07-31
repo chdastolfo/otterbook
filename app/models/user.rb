d4a27b21dfcf30c49c039827ee14a2bd889360b0
Christina D'Astolfo <chdastolfo@gmail.com>
2016-07-31 17:00:28 -0400

    added gravatars

--- app/models/user.rb
+++ app/models/user.rb
@@ -19,4 +19,12 @@
     "#{first_name.to_s} " " #{last_name.to_s}"
   end
 
+  def gravatar_url
+    stripped_email = email.strip
+    downcased_email = stripped_email.downcase
+    hash = Digest::MD5.hexdigest(downcased_email)
+
+    "http://gravatar.com/avatar/#{hash}"
+  end
+
 end
